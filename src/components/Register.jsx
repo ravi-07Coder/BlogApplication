ad35f39f6c9ba29c440b4ef4d231568a6314ef3c
ravi-07Coder <75900722+ravi-07Coder@users.noreply.github.com>
2021-12-03 23:26:08 +0530

    autocpmplete

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -37,11 +37,11 @@ const Register = () => {
         <div className="register">
             {console.log("User", user)}
             <h1 style={{marginTop:185}}>REGISTRATION</h1>
-            <input s tyle={{marginTop:35}}type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
-            <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
-            <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
-            <div  style={{marginRight:185, marginTop:25}} className="button" onClick={register} >Register</div>
+            <input  autoComplete="off"  style={{marginTop:35}}type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
+            <input  autoComplete="off" type="text" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
+            <input autoComplete="off" type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
+            <input autoComplete="off" type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
+            <div   style={{marginRight:185, marginTop:25}} className="button" onClick={register} >Register</div>
         
             <div style={{marginRight:-95, marginTop:-50}} className="button" onClick={()=> {window.location.href="/login"}}>Login</div>
         </div>
